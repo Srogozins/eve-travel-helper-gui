bd9cb9614324d5b517eb1ff2a1bc8a06d15b05dc
Staņislavs Rogozins <stanislavs.rogozins@gmail.com>
2015-04-22 17:43:51 +0300

    Initial System pagination

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,5 @@
 (function () {
-    'use strict';
+  'use strict';
   /**
   * @ngdoc function
   * @name eveTravelHelperApp.controller:MainCtrl
@@ -8,14 +8,27 @@
   * Controller of the eveTravelHelperApp
   */
   angular.module('eveTravelHelperApp')
-  .controller('MainCtrl', ['$scope', '$http', 'ETHConfig', function ($scope, $http, ETHConfig) {
+  .controller('MainCtrl', ['$scope', '$http', '$log', 'ETHConfig', function ($scope, $http, $log, ETHConfig) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    // TODO: implement a resource to handle this
     $http.get(ETHConfig.API_END_POINT + '/systems').success(function(data) {
       $scope.systems = data.systems;
     });
+
+    // TODO: modularise pagination
+    $scope.itemsPerPage = 20;
+    $scope.maxSize = 10;
+    $scope.totalItems = 8030;
+
+    $scope.pageChanged = function() {
+      $log.log('Page changed to: ' + $scope.currentPage);
+      $http.get(ETHConfig.API_END_POINT + '/systems/page/' + $scope.currentPage + '?per_page=' + $scope.itemsPerPage).success(function(data) {
+        $scope.systems = data.systems;
+      });
+    };
   }]);
 }());
