a633529d7f9e166ec9b79dc2521f00a093b6c312
Staņislavs Rogozins <stanislavs.rogozins@gmail.com>
2015-05-01 20:16:32 +0300

    Directive test

--- test/spec/directives/ethpaged.js
+++ test/spec/directives/ethpaged.js
@@ -4,17 +4,41 @@ describe('Directive: ethPaged', function () {
 
   // load the directive's module
   beforeEach(module('eveTravelHelperApp'));
+  beforeEach(module('ethMock'));
 
   var element,
-    scope;
+    scope,
+    q;
 
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function ($q, $rootScope) {
     scope = $rootScope.$new();
+    scope.listFn = function(page){};
+    scope.extractFn = function(data){};
+    q = $q;
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<eth-paged></eth-paged>');
+  it('should use supplied paginated request and data extraction methods to update scope variable with paginated data', inject(function ($compile) {
+    scope.items = [];
+
+    // Automatic request for first page
+    spyOn(scope, 'listFn').and.callFake(function() {
+        var deferred = q.defer();
+        deferred.resolve({data: 'D1'});
+        return deferred.promise;
+    });
+    spyOn(scope, 'extractFn').and.returnValue('S1');
+    element = angular.element('<eth-paged items="items" list-fn="listFn(page)" extract-fn="extractFn(data)"></eth-paged>');
     element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the ethPaged directive');
+
+    expect(scope.listFn).toHaveBeenCalledWith(1);
+    scope.$digest();
+    expect(scope.extractFn).toHaveBeenCalledWith('D1');
+    expect(scope.items).toEqual('S1');
+
+    element.isolateScope().currentPage = 2;
+    element.isolateScope().$digest();
+
+
+
   }));
 });
